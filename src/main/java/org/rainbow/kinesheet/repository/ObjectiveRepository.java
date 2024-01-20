a2a4478dd56894bcf197f599b51c8bb06e539919
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-20 04:43:09 -0500

    Change objective repository Id type

--- src/main/java/org/rainbow/kinesheet/repository/ObjectiveRepository.java
+++ src/main/java/org/rainbow/kinesheet/repository/ObjectiveRepository.java
@@ -1,8 +1,10 @@
 package org.rainbow.kinesheet.repository;
 
+import java.util.UUID;
+
 import org.rainbow.kinesheet.model.Objective;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ObjectiveRepository extends CrudRepository<Objective, String> {
+public interface ObjectiveRepository extends CrudRepository<Objective, UUID> {
     
 }
