9d0fd9063f1cd88942efeaabdd576d6ca597b335
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-21 17:35:21 -0500

    Add findById test cases

--- src/test/java/org/rainbow/kinesheet/controller/ObjectiveControllerTest.java
+++ src/test/java/org/rainbow/kinesheet/controller/ObjectiveControllerTest.java
@@ -29,11 +29,34 @@ class ObjectiveControllerTest {
 		String id = "350bebae-d54f-4e60-a2c8-77a0778e1c5b";
 		String path = "/objectives/" + id;
 		String token = jwtTokenGenerator.generate();
-		
+
 		mvc.perform(get(path).header("Authorization", "Bearer " + token))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.id").value(id))
 				.andExpect(jsonPath("$.title").value("Read Heavenly Mathematics"));
 	}
 
+	@Test
+	@WithMockUser(username = "user2")
+	void findById_ObjectiveDoesNotBelongToRequester_Return403() throws Exception {
+		// The objective identified by this id belongs to user1 whose email is
+		// user1@company.com. So we should get a 403 if user2 requests for it.
+		String id = "350bebae-d54f-4e60-a2c8-77a0778e1c5b";
+		String path = "/objectives/" + id;
+		String token = jwtTokenGenerator
+				.generate(customer -> customer.claim("name", "user2").claim("email", "user2@company.com"));
+
+		mvc.perform(get(path).header("Authorization", "Bearer " + token)).andExpect(status().isForbidden());
+	}
+
+	@Test
+	@WithMockUser(username = "user1")
+	void findById_ObjectiveDoesNotExit_Return404() throws Exception {
+		String id = "bdbd694c-d820-4f9f-a409-17210ce53038";
+		String path = "/objectives/" + id;
+		String token = jwtTokenGenerator.generate();
+
+		mvc.perform(get(path).header("Authorization", "Bearer " + token)).andExpect(status().isNotFound());
+	}
+	
 }
