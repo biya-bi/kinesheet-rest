dd33808bc2893756f89cc0818d3d2889a8b5010f
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-01-21 20:36:01 -0500

    Add a foreign key constraint the objective table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,5 +9,6 @@ CREATE TABLE objective
 (
     id          VARCHAR(64) DEFAULT random_uuid() PRIMARY KEY,
     title       VARCHAR(512) NOT NULL,
-    achiever_id VARCHAR(64) NOT NULL
+    achiever_id VARCHAR(64) NOT NULL,
+    FOREIGN KEY (achiever_id) REFERENCES achiever(id)
 );
